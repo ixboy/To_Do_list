433085137219dca8b27d0c895ea2048c4c188423
ixboy <ismaelixboy@gmail.com>
2021-08-25 10:14:25 +0100

    fix error webpack-server not working

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,30 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    index: './src/app.js',
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,    
   },
+
   devServer: {
-    contentBase: './dist'
+    static: {
+      directory: 'dist',
+    },
+    compress: true,
+    port: 9000,
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
+  
   module: {
     rules: [
       {
