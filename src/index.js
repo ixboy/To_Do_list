e780dd9c8aa84f2eeee7a8ff6cc7a35d516c21cf
ixboy <ismaelixboy@gmail.com>
2021-08-27 04:51:30 +0100

    add addTodo function and listItems

--- src/index.js
+++ src/index.js
@@ -6,32 +6,36 @@ import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 import './style.css';
 
+const clear = document.querySelector('.fa-sync');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
 const list = document.querySelector('#list');
-const toDoList = [
-  {
-    description: 'Eat breakfast today',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'watch video tutorials',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'attend microverse classes',
-    completed: false,
-    index: 2,
-  },
-];
+const toDoList = [];
 
-function addItems() {
+function listItems() {
   list.innerHTML = '';
   toDoList.forEach((e) => {
     list.innerHTML += `<li>
+    <i class="de fa fa-check-circle" job="complete" id="${e.id}"></i>
     <a class="text"> ${e.description} </a>
-    <i class="de fa fa-trash"></i>
+    <i class="de fa fa-trash" job="delete" id="${e.id}"></i>
   </li>`;
   });
+  
 }
-window.addEventListener('DOMContentLoaded', addItems());
+
+function addToDo(e) {
+  e.preventDefault();
+  if (input.value) {
+    toDoList.push({
+      description: input.value,
+      completed: false,
+      id: toDoList.length,
+    });
+    listItems();
+    input.value = '';
+  }
+}
+
+window.addEventListener('DOMContentLoaded', listItems());
+btn.addEventListener('click', addToDo);
