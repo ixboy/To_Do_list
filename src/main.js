a313a0b38745205771c7e723d7a2477d1590c83c
ixboy <ismaelixboy@gmail.com>
2021-08-28 10:32:16 +0100

    refactored code

--- src/main.js
+++ src/main.js
@@ -1,15 +1,48 @@
-// /* eslint-disable func-names */
-// // eslint-disable-next-line import/no-cycle
-// import updateLocalStorage from './index';
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-cycle */
+import listItems from './checks';
 
-// export default function chechekInput(todo) {
-//   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const dateElement = document.getElementById('date');
+// const clear = document.querySelector('.fa-sync');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+const clearAllCompleted = document.querySelector('.clear-completed');
+let toDoList = [];
 
-//   checkboxes.forEach((checkbox) => checkbox.addEventListener('change', function () {
-//     if (this.checked) {
-//       todo[this.id].completed = true;
-//       updateLocalStorage();
-//     } else todo[this.id].completed = false;
-//     updateLocalStorage();
-//   }));
-// }
+if (localStorage.toDoList !== undefined) {
+  toDoList = JSON.parse(localStorage.toDoList);
+}
+
+const options = { weekday: 'long', month: 'short', day: 'numeric' };
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString('en-US', options);
+
+export default function updateLocalStorage() {
+  localStorage.toDoList = JSON.stringify(toDoList);
+}
+
+function clearCompleted(e) {
+  e.preventDefault();
+  toDoList = toDoList.filter((todo) => (todo.completed === false));
+  updateLocalStorage();
+  listItems(toDoList);
+}
+
+function addToDo(e) {
+  e.preventDefault();
+  if (input.value) {
+    toDoList.push({
+      description: input.value,
+      completed: false,
+      id: toDoList.length,
+    });
+    listItems(toDoList);
+    input.value = '';
+  }
+}
+
+window.addEventListener('DOMContentLoaded', listItems(toDoList));
+btn.addEventListener('click', addToDo);
+clearAllCompleted.addEventListener('click', clearCompleted);
