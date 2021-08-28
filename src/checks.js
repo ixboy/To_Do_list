397b76a1824ad0b10b00e40c0509a21368baf755
ixboy <ismaelixboy@gmail.com>
2021-08-28 22:43:55 +0100

    create function to edit description

--- src/checks.js
+++ src/checks.js
@@ -15,7 +15,7 @@ export default function listItems() {
   toDoList.forEach((e) => {
     list.innerHTML += `<li class="list-all">
       <input type="checkbox" class="${e.completed ? 'completed' : ''}" id="${e.index}">
-      <a class="text"> ${e.description} </a>
+      <input type="text" value="${e.description}" class="edit-input description-${e.index}">
       <button class="del" type="submit" id="${e.index}><i class="fas fa-trash"></i></button> 
     </li>`;
   });
@@ -23,23 +23,39 @@ export default function listItems() {
   const listAll = document.querySelectorAll('.list-all');
   listAll.forEach((li, i) => {
     const button = li.lastElementChild;
+    const input = li.children[1];
     const checkbox = li.firstElementChild;
+
+    input.addEventListener('keyup', (e) => {
+      e.preventDefault();
+      if (e.key === 'Enter') {
+        const newDescrip = e.target.value;
+        toDoList[i].description = newDescrip;
+        localStorage.toDoList = JSON.stringify(toDoList);
+      }
+    });
+
     button.addEventListener('click', (e) => {
       e.target.parentElement.remove();
       toDoList.splice(i, 1);
       updateIndex();
     });
+
     checkbox.addEventListener('click', (e) => {
       if (e.target.checked) {
         toDoList[i].completed = true;
         e.target.checked = true;
         e.target.parentElement.classList.add('completed');
-        localStorage.toDoList = JSON.stringify(toDoList);
+        updateLocalStorage();
+
+        // localStorage.toDoList = JSON.stringify(toDoList);
       } else {
         toDoList[i].completed = false;
         e.target.checked = false;
         e.target.parentElement.classList.remove('completed');
-        localStorage.toDoList = JSON.stringify(toDoList);
+        updateLocalStorage();
+
+        // localStorage.toDoList = JSON.stringify(toDoList);
       }
     });
   });
